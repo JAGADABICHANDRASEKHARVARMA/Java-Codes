6da5aefcdd8974c328967dc4bab5af8cebb4f940
JAGADABI CHANDRASEKHAR VARMA <68846646+JAGADABICHANDRASEKHARVARMA@users.noreply.github.com>
2021-03-29 17:13:28 +0530

    Create WrapperClassDemo.java

---
+++ WrapperClassDemo.java
@@ -0,0 +1,18 @@
+
+
+public class WrapperClassDemo {
+    public static void main(String[] args) {
+        int i = 5; //primitive datatype
+
+        Integer ii = new Integer(5); // boxing - Wrapping
+
+        int j = ii.intValue(); // unboxing - Unwrapping
+
+        Integer value = i; // AutoBoxing - AutoWrapping
+
+        int k = i; // AutoUnBoxing - AutoUnWrapping
+
+
+
+    }
+}
