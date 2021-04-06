7a03c8006c8007667e7c0b4dc5972ba9b892131b
JAGADABI CHANDRASEKHAR VARMA <68846646+JAGADABICHANDRASEKHARVARMA@users.noreply.github.com>
2021-04-06 20:52:27 +0530

    Create IssueWithMultipleInheritanceInInterface.java

---
+++ IssueWithMultipleInheritanceInInterface.java
@@ -0,0 +1,33 @@
+interface Zero{
+    void abc();
+    default void show(){
+        System.out.println("In Zero");
+    }
+}
+interface MyZero{
+    default void show(){
+        System.out.println("In MyZero");
+    }
+
+}
+class ImpZero implements Zero, MyZero{
+
+    public void abc() {
+        System.out.println("in abc");
+    }
+
+    @Override
+    public void show() {
+        MyZero.super.show();
+    }
+}
+
+public class IssueWithMultipleInheritanceInInterface {
+    public static void main(String[] args) {
+        Zero obj = new ImpZero();
+
+        obj.abc();
+        obj.show();
+
+    }
+}
