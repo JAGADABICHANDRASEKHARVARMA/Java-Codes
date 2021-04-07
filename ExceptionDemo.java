74b5e9273ff27064083bb760e394da07605e3d78
JAGADABI CHANDRASEKHAR VARMA <68846646+JAGADABICHANDRASEKHARVARMA@users.noreply.github.com>
2021-04-07 20:39:42 +0530

    Update ExceptionDemo.java

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -3,13 +3,18 @@ package com.chandu;
 public class ExceptionDemo {
     public static void main(String[] args) {
         try{
-            int i = 9/0;
+            int i = 8;
+            int j = 0;
+            int k = i/j;
+            System.out.println("Output is "+k);
 
         }
-        catch (Exception e)
+        catch (ArithmeticException e)
         {
             System.err.println("Error");
         }
-        System.out.println("Bye");
+        finally {
+            System.out.println("Bye");
+        }
     }
 }
